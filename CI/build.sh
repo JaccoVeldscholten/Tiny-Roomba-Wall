3d98dcbb82f4895ee0f7842524e6f1527830db75
Jacco Veldscholten <jjveldscholten@gmail.com>
2021-08-02 22:01:26 +0200

    Added compiling

--- CI/build.sh
+++ CI/build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-sudo apt get update -y
-sudo apt-get install -y gcc-avr
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Install packages we need for building
+sudo apt-get update -y
+sudo apt-get install -y gcc-avr make cmake gcc
+
+# Make sure we are inside the github workspace
+cd $GITHUB_WORKSPACE
+
+# Compile
+avr-gcc -Wall -g -Os -mmcu=attiny85 -o main.bin main.c
+avr-objcopy -j .text -j .data -O ihex main.bin main.hex
